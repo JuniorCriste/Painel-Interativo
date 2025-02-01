6cef895d609312e2fdb9236f23b2b0f2f5e8acd1
juniorcriste <informaticode@gmail.com>
2025-02-01 17:46:41 -0300

    0.2

--- interativo.py
+++ interativo.py
@@ -1,14 +1,11 @@
 import flet as ft
 
-class PainelInterativo:
-    def __init__(self, page: ft.Page):
-        self.Page = page
-        self.bgcolor = ft.colors.BLUE_300
-        self.page.window_full_screen = True
-        self.page.window_aways_on_top = True
-        self.page.title = "Painel Interativo - 100% Capixabas!"
-
-        self.page.update()
+def PainelInterativo(page: ft.Page):
+    page.bgcolor = ft.colors.BLUE_300
+    page.window_full_screen = True
+    page.window_aways_on_top = True
+    page.title = "Painel Interativo - 100% Capixabas!"
+    page.update()
 
 
 ft.app(target=PainelInterativo)        
