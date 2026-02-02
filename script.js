91cdb530386c42c4e3de06a1ead7f4a5b0b930cf
JuniorCriste <informaticode@gmail.com>
2026-02-01 21:59:43 -0300

    1.0

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ setInterval(autoSlide, 5000);
 document.addEventListener('keydown', (e) => {
     const keyLinks = {
         "1": "https://juniorcriste.github.io/QuizCapixaba/",
-        "2": "#", "3": "#", "4": "#", "5": "pages/opentictactoe/play.html",
-        "6": "#", "7": "#", "8": "#", "9": "pages/geniomatematico/play.html"
+        "2": "#", "3": "#", "4": "#", "5": "apps/opentictactoe/play.html",
+        "6": "#", "7": "#", "8": "#", "9": "apps/geniomatematico/play.html"
     };
 
     if (keyLinks[e.key]) {
